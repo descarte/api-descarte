b07ae5ed3934765181b66d9ac8f5b1295cefd7ff
Centeno <centeno@diegocenteno.com>
2015-04-11 16:06:18 -0300

    Add remote postgres

--- model/index.js
+++ model/index.js
@@ -3,8 +3,27 @@
 var fs        = require("fs");
 var path      = require("path");
 var Sequelize = require("sequelize");
-var sequelize = new Sequelize(process.env.DATABASE_URL || "postgres://centeno@localhost/centeno");
+//var connection = process.env.DATABASE_URL || "postgres://centeno@localhost/centeno";
+var connection = process.env.DATABASE_URL || "postgres://@ec2-107-20-159-103.compute-1.amazonaws.com:5432/d6333mgostb22p";
+/*var sequelize = new Sequelize(connection, { dialectOptions: {
+        ssl: true
+    }});*/
+
+
+
+var sequelize = new Sequelize('d6333mgostb22p', 'jqlcwwvwcqpsdb', 'zw-8_5r-MJJAeaQ4r4UgPGEyWy', {
+  host: 'ec2-107-20-159-103.compute-1.amazonaws.com',
+  dialect: 'postgres',
+  port: 5432,
+    dialectOptions: {
+        ssl: true
+    }
+});
+
+
+
 var db        = {};
+
 
 fs
 	.readdirSync(__dirname)
